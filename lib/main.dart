d435cc15926994c8b2a5cf5fa49718ee83537ff6
zhangty <zhangty@szlanyou.com>
2020-11-16 22:39:35 +0800

    submit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wanandroid/manager/app_manager.dart';
+import 'package:flutter_wanandroid/ui/widget/main_drawer.dart';
+import 'ui/page/page_article.dart';
+
+void main() => runApp(new ArticleApp());
+
+class ArticleApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    AppManager.initApp();
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(
+            '文章',
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        drawer: Drawer(
+          child: MainDrawer(),
+        ),
+        body: new ArticlePage(),
+      ),
+    );
+  }
+
+
+}
